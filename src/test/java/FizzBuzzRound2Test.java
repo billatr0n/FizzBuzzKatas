52a4cbf0449ae57f10ce35698c6f82ccc5d2220a
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-16 10:03:58 +0200

    Add failing test scenario for FizzBuzz

--- src/test/java/FizzBuzzRound2Test.java
+++ src/test/java/FizzBuzzRound2Test.java
@@ -7,11 +7,17 @@ import org.junit.Test;
 public class FizzBuzzRound2Test {
     @Test
     public void getResultDividableByThree(){
-        Assert.assertEquals("fizz", FizzBuzzRound2.getResult(3));
+        Assert.assertEquals("Fizz", FizzBuzzRound2.getResult(3));
     }
 
     @Test
     public void getResultDividableByFive(){
-        Assert.assertEquals("buzz", FizzBuzzRound2.getResult(5));
+        Assert.assertEquals("Buzz", FizzBuzzRound2.getResult(5));
+        Assert.assertEquals("Buzz", FizzBuzzRound2.getResult(10));
+    }
+    @Test
+    public void getResultDividableByBoth(){
+        Assert.assertEquals("FizzBuzz", FizzBuzzRound2.getResult(15));
+        Assert.assertEquals("FizzBuzz", FizzBuzzRound2.getResult(30));
     }
 }
