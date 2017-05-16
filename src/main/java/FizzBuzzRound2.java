51958c566539cd3ad90c8fd6c4a07bc215f56644
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-16 09:57:48 +0200

    Refactor

--- src/main/java/FizzBuzzRound2.java
+++ src/main/java/FizzBuzzRound2.java
@@ -2,8 +2,19 @@
  * Created by vasilis on 16-5-17.
  */
 public class FizzBuzzRound2 {
+
+    public static String FIZZ = "fizz";
+
     public static String getResult(Integer value){
-        if(value % 3 == 0) return "fizz";
+        if(isDividableByThree(value))
+            return FIZZ;
+
         return value.toString();
+    }
+    public static boolean isDividableByThree(Integer value){
+        if(value % 3 == 0){
+            return true;
+        }
+        return false;
     }
 }
