a66369c5c3eac83eafb49bbb69c1818b380123f7
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-16 10:05:57 +0200

    Add implementation for FizzBuzz

--- src/main/java/FizzBuzzRound2.java
+++ src/main/java/FizzBuzzRound2.java
@@ -3,14 +3,17 @@
  */
 public class FizzBuzzRound2 {
 
-    public static String FIZZ = "fizz";
-    public static String BUZZ = "buzz";
+    public static String FIZZ = "Fizz";
+    public static String BUZZ = "Buzz";
+    public static String FIZZBUZZ = "FizzBuzz";
 
     public static String getResult(Integer value){
         if(isDividableByThree(value))
             return FIZZ;
         else if(isDividableByFive(value))
             return BUZZ;
+        else if(isDividableByBoth(value))
+            return FIZZBUZZ;
         return value.toString();
     }
     public static boolean isDividableByThree(Integer value){
@@ -22,6 +25,13 @@ public class FizzBuzzRound2 {
 
     public static boolean isDividableByFive(Integer value){
         if(value % 5 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isDividableByBoth(Integer value){
+        if(value % 15 == 0){
             return true;
         }
         return false;
