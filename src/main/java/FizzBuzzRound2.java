44c045f5b1d4ae17eedae263bb4c5cdf1bdb26f9
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-16 10:01:04 +0200

    Add implementation for divisions with 5

--- src/main/java/FizzBuzzRound2.java
+++ src/main/java/FizzBuzzRound2.java
@@ -4,15 +4,24 @@
 public class FizzBuzzRound2 {
 
     public static String FIZZ = "fizz";
+    public static String BUZZ = "buzz";
 
     public static String getResult(Integer value){
         if(isDividableByThree(value))
             return FIZZ;
-
+        else if(isDividableByFive(value))
+            return BUZZ;
         return value.toString();
     }
     public static boolean isDividableByThree(Integer value){
         if(value % 3 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isDividableByFive(Integer value){
+        if(value % 5 == 0){
             return true;
         }
         return false;
