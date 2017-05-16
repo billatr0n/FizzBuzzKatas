c206e92ce9df0368507209186029ba0aee2b2197
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-16 09:53:38 +0200

    Introduce first implementation

--- src/main/java/FizzBuzzRound2.java
+++ src/main/java/FizzBuzzRound2.java
@@ -2,4 +2,8 @@
  * Created by vasilis on 16-5-17.
  */
 public class FizzBuzzRound2 {
+    public static String getResult(Integer value){
+        if(value % 3 == 0) return "fizz";
+        return value.toString();
+    }
 }
